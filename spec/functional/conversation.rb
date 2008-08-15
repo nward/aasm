bdfe6b0464544f3e7089602bfba1dda0dac8b544
Greg Vaughn <gvaughn@thinkrelevance.com>
2008-08-15 10:45:09 -0500

    Revert "Failing test for arguments received by event method being passed to on_transition method" Now I understand what the feature is supposed to do. It's counter-intuitive though. This reverts commit 45fcefe311c48d8ede8237f6232f69826f181874.

--- spec/functional/conversation.rb
+++ spec/functional/conversation.rb
@@ -31,26 +31,19 @@ class Conversation
 
   aasm_event :unjunk do
   end
-  
-  aasm_event :args_test do
-    transitions :to => :junk, :from => [:needs_attention], :on_transition => :transition_method
-  end
 
   def initialize(persister)
     @persister = persister
   end
 
-  def transition_method(*args)
-    p "transition_method: #{args}"
-  end
 
   private
   def aasm_read_state
-    @persister.state
+    @persister.read_state
   end
 
   def aasm_write_state(state)
-    @persister.state = state
+    @persister.write_state(state)
   end
 
 end
--- spec/functional/conversation_spec.rb
+++ spec/functional/conversation_spec.rb
@@ -2,11 +2,4 @@ describe Conversation, 'description' do
   it '.aasm_states should contain all of the states' do
     Conversation.aasm_states.should == [:needs_attention, :read, :closed, :awaiting_response, :junk]
   end
-  
-  it 'should pass arguments received by event method to :on_transition method' do
-    c = Conversation.new(Struct.new(:state).new)
-    args = [nil, 1, 'a', Object.new]
-    c.should_receive(:transition_method).with(*args)
-    c.args_test(*args)
-  end
 end
